8e57d9a5ba0de6c9faaeece1921254d3b101acc7
Sureka <sureka0107@gmail.com>
2022-03-09 21:01:58 +0530

    first commit

---
+++ youtube.js
@@ -0,0 +1,96 @@
+/*function setNewImage1()
+{
+    document.getElementById("img1").src="https://i.ytimg.com/vi/jRFKZGWrmrM/maxresdefault.jpg"; oooooo
+}
+function setOldImage1()
+{
+    document.getElementById("img1").src="https://i.ytimg.com/vi/r9lR5hLgEnI/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBB3tNZ-tv_c1CQh1u-YoT_ywzz3w";
+}
+function setNewImage2()
+{ 
+    document.getElementById("img2").src="https://code.visualstudio.com/assets/docs/languages/javascript/overview.png"; oooooo
+}
+function setOldImage2()
+{
+    document.getElementById("img2").src="https://i.ytimg.com/vi/ATElufr0OiE/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCJuaATtZxg3dXNAyUiTBM-dVbiOA";
+}
+function setNewImage3()
+{
+    document.getElementById("img3").src="https://images.idgesg.net/images/article/2019/03/javaworld_jvm_jdk_jre_explainer_java_development_kit_write_3x2_2400x1600_by_idg_oracle_vasabii_gettyimages-100790551-large.jpg?auto=webp&quality=85,70"; ooooo
+}
+function setOldImage3()
+{
+    document.getElementById("img3").src="https://i.ytimg.com/vi/G5ur_C-L7ZU/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLChu2FKD3CYttkRG1m739X7tdi8MQ";
+}
+function setNewImage4()
+{
+     document.getElementById("img4").src="https://www.habilelabs.io/wp-content/uploads/2021/02/This-is-Why-You-Should-Choose-React-js-1.jpg"; ooooo
+}
+function setOldImage4()
+{ 
+    document.getElementById("img4").src="https://i.ytimg.com/vi/jmEJsv9WT8s/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDJQwLQeymT53HaC_fZ5LBOJ0G6fQ"; 
+}
+function setNewImage5()
+{
+    document.getElementById("img5").src="https://merehead.com/blog/wp-content/uploads/11-5.png"; ooooo
+}
+function setOldImage5()
+{
+    document.getElementById("img5").src="https://i.ytimg.com/vi/fRD_3vJagxk/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAEYn21IC3IEXU-3mSFIsWUzouLtQ";
+}
+function setNewImage6()
+{
+ document.getElementById("img6").src="https://static.javatpoint.com/tutorial/git/images/git-benefits.png"; oooo 
+}
+function setOldImage6()
+{ 
+    document.getElementById("img6").src="https://i.ytimg.com/vi/gjOLk0L830c/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDvZZweHjjjcOKMR_jA3E8NryqvQg";
+}
+function setNewImage7()
+{
+    document.getElementById("img7").src="https://dotnettrickscloud.blob.core.windows.net/img/angular/angular-component-page-view.png"; oooo
+}
+function setOldImage7()
+{
+    document.getElementById("img7").src="https://i.ytimg.com/vi/00fWlZnZAo0/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBwEtGQktNejGrNI2dqq9Ngt0UibA";
+}
+function setNewImage8()
+{ 
+    document.getElementById("img8").src="https://cdn-media-1.freecodecamp.org/images/1*4psHn7qR2v1O5kBhJQ_T2w.png"; oooooo
+}
+function setOldImage8()
+{
+    document.getElementById("img8").src="https://i.ytimg.com/vi/A_z5g0_hJN8/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCief-B1bUkymcxXaMHrwAa4psRmA"; 
+}
+function setNewImage9()
+{ 
+    document.getElementById("img9").src="https://cdn-media-1.freecodecamp.org/images/1*4psHn7qR2v1O5kBhJQ_T2w.png"; oooooo
+}
+function setOldImage9()
+{
+    document.getElementById("img9").src="https://i.ytimg.com/vi/Dcr2Rp2ynJI/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBXmBj4AXyPza65IJyp6c4mLWn4tg"; 
+}
+function setNewImage10()
+{ 
+    document.getElementById("img10").src="https://cdn-media-1.freecodecamp.org/images/1*4psHn7qR2v1O5kBhJQ_T2w.png"; oooooo
+}
+function setOldImage10()
+{
+    document.getElementById("img10").src="https://i.ytimg.com/vi/Dcr2Rp2ynJI/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBXmBj4AXyPza65IJyp6c4mLWn4tg"; 
+}
+function setNewImage11()
+{ 
+    document.getElementById("img11").src="https://cdn-media-1.freecodecamp.org/images/1*4psHn7qR2v1O5kBhJQ_T2w.png"; oooooo
+}
+function setOldImage11()
+{
+    document.getElementById("img11").src="https://i.ytimg.com/vi/ZBO8W4wmLQc/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA2UOnZP1srqgDunMGZ4UVhX_YyFg"; 
+}
+function setNewImage12()
+{ 
+    document.getElementById("img12").src="https://cdn-media-1.freecodecamp.org/images/1*4psHn7qR2v1O5kBhJQ_T2w.png"; oooooo
+}
+function setOldImage12()
+{
+    document.getElementById("img12").src="https://i.ytimg.com/vi/Gi83R8jEqZU/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCMsOwNCzU3GZHzlIz5CgajhWgh9Q"; 
+}*/
